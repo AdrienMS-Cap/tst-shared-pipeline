7afb407011521c8ff47d4a0a2b74c353cef1b301
Adrien MENDES SANTOS <adrien.mendes-santos@capgemini.com>
2024-01-09 15:00:52 +0100

    k

--- scripts/scan_secrets.py
+++ scripts/scan_secrets.py
@@ -1,35 +1,9 @@
-import os
 import re
-import sys
+import os
+import argparse
 
-def find_sensitive_info(file_path, sensitive_patterns):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        print("Processing file: {}".format(file_path))
-        content = file.read()
-        for pattern in sensitive_patterns:
-            matches = re.finditer(rf"{pattern}\s*:\s*(\S+)", content, re.IGNORECASE)
-            for match in matches:
-                line_number = content.count('\n', 0, match.start()) + 1
-                print("ALERT - Sensitive information found in file: {}, Line {}".format(file_path, line_number))
-                return True
-    return False
-
-def scan_for_sensitive_info(sensitive_patterns, script_file_path):
-    error_found = False
-
-    for root, dirs, files in os.walk("."):
-        # Exclude the script file and directories/files that start with a dot
-        dirs[:] = [d for d in dirs if not d.startswith('.') and not os.path.samefile(os.path.join(root, d), script_file_path)]
-        files = [f for f in files if not f.startswith('.') and not os.path.samefile(os.path.join(root, f), script_file_path)]
-
-        for file in files:
-            file_path = os.path.join(root, file)
-            if find_sensitive_info(file_path, sensitive_patterns):
-                error_found = True
-
-    return error_found
-
-def main():
+def scan_directory(path: str) -> None:
+    """Scan a directory for sensitive data."""
     sensitive_patterns = [
         "password",
         "pwd",
@@ -49,15 +23,27 @@ def main():
         "ssh-rsa",
     ]
 
-    # Get the path of the script file
-    script_file_path = os.path.abspath(__file__)
+    regex_patterns = [re.compile(rf'{pattern}.\s?[a-zA-Z0-9]+') for pattern in sensitive_patterns]
 
-    error_found = scan_for_sensitive_info(sensitive_patterns, script_file_path)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            try:
+                with open(file_path, 'r') as f:
+                    print("Scanning file: {}.".format(file_path))
+                    contents = f.read()
+                    for pattern in regex_patterns:
+                        if pattern.search(contents):
+                            print("Possible sensitive data found in file: {}".format(file_path))
+            except Exception as e:
+                print("Could not read file {}. Reason: {}".format(file_path, str(e)))
 
-    if error_found:
-        sys.exit(1)
-    else:
-        print("No sensitive information found.")
+def main():
+    """Main function to parse arguments and call the scan_directory function."""
+    parser = argparse.ArgumentParser(description='Scan a directory for sensitive data.')
+    parser.add_argument('path', type=str, help='The path to the directory you want to scan.')
+    args = parser.parse_args()
+    scan_directory(args.path)
 
 if __name__ == "__main__":
     main()
