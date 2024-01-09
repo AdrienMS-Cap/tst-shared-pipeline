88a69056b539633eaad0d129bd9f6b2f2892b0a9
Adrien MENDES SANTOS <adrien.mendes-santos@capgemini.com>
2024-01-09 15:10:17 +0100

    l

--- scripts/scan_secrets.py
+++ scripts/scan_secrets.py
@@ -1,6 +1,7 @@
 import re
 import os
 import argparse
+import sys
 
 def scan_directory(path: str) -> None:
     """Scan a directory for sensitive data."""
@@ -24,19 +25,31 @@ def scan_directory(path: str) -> None:
     ]
 
     regex_patterns = [re.compile(rf'{pattern}.\s?[a-zA-Z0-9]+') for pattern in sensitive_patterns]
+    secrets_found = []
 
     for root, dirs, files in os.walk(path):
+        # Ignore directories that start with a dot
+        dirs[:] = [d for d in dirs if not d.startswith('.')]
+        
         for file in files:
             file_path = os.path.join(root, file)
             try:
                 with open(file_path, 'r') as f:
                     print("Scanning file: {}.".format(file_path))
-                    contents = f.read()
-                    for pattern in regex_patterns:
-                        if pattern.search(contents):
-                            print("Possible sensitive data found in file: {}".format(file_path))
+                    lines = f.readlines()
+                    for i, line in enumerate(lines, 1):
+                        for pattern in regex_patterns:
+                            if pattern.search(line):
+                                print("Possible sensitive data found in file: {}".format(file_path))
+                                secrets_found.append((file_path, i))
             except Exception as e:
                 print("Could not read file {}. Reason: {}".format(file_path, str(e)))
+
+    if secrets_found:
+        print("\nSummary of files with possible sensitive data:")
+        for file_path, line_num in secrets_found:
+            print(f"File: {file_path}, Line: {line_num}")
+        sys.exit("Error: Secrets were found during the process.")
 
 def main():
     """Main function to parse arguments and call the scan_directory function."""
